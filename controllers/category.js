9d9ab04d70a059d6d9c522329226d900a408fff9
kiran <gandikotakiran99@gmail.com>
2020-06-03 15:01:14 +0530

    cbetsht

---
+++ controllers/category.js
@@ -0,0 +1,69 @@
+const Category = require("../models/category");
+
+exports.getCategoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, cate) => {
+    if (err) {
+      return res.status(400).json({
+        err: "Category not found in DB"
+      });
+    }
+    req.category = cate;
+    next();
+  });
+};
+
+exports.createCategory = (req, res) => {
+  const category = new Category(req.body);
+  category.save((err, category) => {
+    if (err) {
+      return res.status(400).json({
+        err: "NOT able to save category in DB"
+      });
+    }
+    res.json({ category });
+  });
+};
+
+exports.getCategory = (req, res) => {
+  return res.json(req.category);
+};
+
+exports.getAllCategory = (req, res) => {
+  Category.find().exec((err, categories) => {
+    if (err) {
+      return res.status(400).json({
+        err: "NO categories found"
+      });
+    }
+    res.json(categories);
+  });
+};
+
+exports.updateCategory = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+
+  category.save((err, updatedCategory) => {
+    if (err) {
+      return res.status(400).json({
+        err: "Failed to update category"
+      });
+    }
+    res.json(updatedCategory);
+  });
+};
+
+exports.removeCategory=(req,res)=>{
+    const category= req.category;
+    //remove is a function from mongoose
+    category.remove((err,category)=>{
+        if(err){
+            return res.status(400).json({
+                err:"Failed to delete category"
+            })
+        }
+        res.json({
+            message:"Successfully deleted"
+        })
+    })
+}
